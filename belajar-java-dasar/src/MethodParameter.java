fa80f58ea2a8610720c4e2c0733cc6bc44e9b04a
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-02-21 20:14:12 +0700

    basic 21 # Method Parameter

---
+++ belajar-java-dasar/src/MethodParameter.java
@@ -0,0 +1,9 @@
+public class MethodParameter {
+    static void main(String[] args) {
+        sayHello("bachtiar", "firdaus");
+        sayHello("indra", "kurniawan");
+    }
+    static void sayHello(String firstName, String lastName){
+        System.out.println("Hello " + firstName + " " + lastName);
+    }
+}
