7cc2586586ecf919afec7e719f76cde6444a6c1e
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-02-21 14:52:38 +0700

    basic 3 # tipe data char

---
+++ belajar-java-dasar/src/TipeDataChar.java
@@ -0,0 +1,18 @@
+public class TipeDataChar {
+    static void main(String[] args) {
+        char d = 'D';
+        char a = 'A';
+        char u = 'U';
+        char s = 'S';
+
+        System.out.println(d);
+        System.out.println(a);
+        System.out.println(u);
+        System.out.println(s);
+
+        System.out.print(d);
+        System.out.print(a);
+        System.out.print(u);
+        System.out.print(s);
+    }
+}
