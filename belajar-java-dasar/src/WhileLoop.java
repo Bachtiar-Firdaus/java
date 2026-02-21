8ca7f19e141a44ea7f8b8b2ec79cc488b53e94d7
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-02-21 16:54:38 +0700

    basic 16 # while loop

---
+++ belajar-java-dasar/src/WhileLoop.java
@@ -0,0 +1,9 @@
+public class WhileLoop {
+    static void main(String[] args) {
+        var counter = 1;
+        while (counter <= 10){
+            System.out.println("perulangan "+ counter);
+            counter++;
+        }
+    }
+}
