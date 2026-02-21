c04e7a9dc28c5699c326e24a027550ab1950ad10
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-02-21 16:57:29 +0700

    basic 17 # do while loop

---
+++ belajar-java-dasar/src/DoWhileLoop.java
@@ -0,0 +1,8 @@
+public class DoWhileLoop {
+    static void main(String[] args) {
+        var counter = 100;
+        do {
+            System.out.println("perulangan ke " + counter);
+        }while (counter <= 10);
+    }
+}
