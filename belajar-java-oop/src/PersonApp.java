3dd6c029a672c0aeb1a0a7986c953aa5d0dedd6d
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-02-22 15:56:49 +0700

    Beginner 3 # Java OOP, Field

--- belajar-java-oop/src/PersonApp.java
+++ belajar-java-oop/src/PersonApp.java
@@ -3,12 +3,17 @@ public class PersonApp {
 
         // menggunakan new untuk membuat object
         var person1 = new Person();
+        person1.name = "Bachtiar";
+        person1.address = "Lampung";
+//        person1.country = "Amerika"; // Error
+        System.out.println(person1.name);
+        System.out.println(person1.address);
+        System.out.println(person1.country);
+
+
         Person person2 = new Person();
         Person person3;
         person3 = new Person();
 
-        System.out.println(person1);
-        System.out.println(person2);
-        System.out.println(person3);
     }
 }
