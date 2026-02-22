763b521103d5584b29c990728d6ce3c8b9c8ebed
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-02-22 20:17:28 +0700

    Beginner 17 # Java OOP, Package

---
+++ belajar-java-oop/src/belajar/pakage/data/Product.java
@@ -0,0 +1,11 @@
+package belajar.pakage.data;
+
+class Product {
+    String name;
+    int price;
+
+    Product(String name, int price){
+        this.name = name;
+        this.price = price;
+    }
+}
