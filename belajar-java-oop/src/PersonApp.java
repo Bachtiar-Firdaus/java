db82a4a6636e529d974bad539ccdf003cdab93b6
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-02-22 15:49:29 +0700

    Beginner 3 # Java OOP, create object

---
+++ belajar-java-oop/src/PersonApp.java
@@ -0,0 +1,14 @@
+public class PersonApp {
+    static void main(String[] args) {
+
+        // menggunakan new untuk membuat object
+        var person1 = new Person();
+        Person person2 = new Person();
+        Person person3;
+        person3 = new Person();
+
+        System.out.println(person1);
+        System.out.println(person2);
+        System.out.println(person3);
+    }
+}
