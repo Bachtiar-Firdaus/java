5b28d0c25e13c93ec333d607d383f0d6409ba2a2
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-02-22 16:44:16 +0700

    Beginner 11 # Java OOP, Method Overidding

--- belajar-java-oop/src/VicePresident.java
+++ belajar-java-oop/src/VicePresident.java
@@ -1,3 +1,8 @@
 // cara mewariskan dengan extend target induk
 class VicePresident extends Manager{
+    // mendeklarasikan ulang padahal di parent sudah ada
+    // ini di namakan method overidding
+    void sayHello(String name){
+        System.out.println("Hi "+ name + ", My Name Is VP " + this.name);
+    }
 }
