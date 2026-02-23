887dfd95469f893baea570c090e9f074aa087f32
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-02-23 19:46:40 +0700

    Beginner 32 # Java OOP, Anonymous Class

--- belajar-java-oop/src/belajar/pakage/app/HelloWorldApp.java
+++ belajar-java-oop/src/belajar/pakage/app/HelloWorldApp.java
@@ -1,4 +1,40 @@
 package belajar.pakage.app;
 
+import belajar.pakage.data.HelloWorld;
+
 public class HelloWorldApp {
+    static void main(String[] args) {
+
+        // ini namanya anonymouse class
+        HelloWorld english = new HelloWorld() {
+
+            public void sayHello() {
+                System.out.println("Hello");
+            }
+
+            public void sayHello(String name) {
+                System.out.println("Hello "+ name);
+            }
+        };
+
+        // ini namanya anonymouse class
+        HelloWorld indonesia = new HelloWorld() {
+
+            public void sayHello() {
+                System.out.println("halo");
+            }
+
+            public void sayHello(String name) {
+                System.out.println("halo "+ name);
+            }
+        };
+
+        // untuk menampilkan perlu dicall functionnya
+        english.sayHello();
+        english.sayHello("bachtiar");
+
+        indonesia.sayHello();
+        indonesia.sayHello("firdaus");
+
+    }
 }
--- belajar-java-oop/src/belajar/pakage/data/HelloWorld.java
+++ belajar-java-oop/src/belajar/pakage/data/HelloWorld.java
@@ -1,4 +1,6 @@
 package belajar.pakage.data;
 
 public interface HelloWorld {
+    void sayHello();
+    void sayHello(String name);
 }
