18022a641598ffe7f5a7b6a68e0f069c0e0d094b
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-02-24 13:53:31 +0700

    Studi Kasus Java OOP: Aplikasi Todolist 5 # Test Repository & Service: Menampilkan TodoList

---
+++ aplikasi-todo-list/src/test/service/TodoListServiceTest.java
@@ -0,0 +1,24 @@
+package test.service;
+
+import entity.TodoList;
+import repository.TodoListRepository;
+import repository.TodoListRepositoryImpl;
+import service.TodoListService;
+import service.TodoListServiceImpl;
+
+public class TodoListServiceTest {
+    static void main(String[] args) {
+        testShowTodoList();
+    }
+
+    public static void testShowTodoList(){
+        TodoListRepositoryImpl todoListRepository = new TodoListRepositoryImpl();
+        todoListRepository.data[0] = new TodoList("Belajar Java Dasar");
+        todoListRepository.data[1] = new TodoList("Belajar Java OOP");
+        todoListRepository.data[2] = new TodoList("Belajar Java Spring");
+
+        TodoListService todoListService = new TodoListServiceImpl(todoListRepository);
+
+        todoListService.showTodoList();
+    }
+}
