7c05f70accbf6feb428b76608a88742b0bfc94bc
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-02-24 14:33:47 +0700

    Studi Kasus Java OOP: Aplikasi Todolist 10 # View: Menampilkan TodoList

--- aplikasi-todo-list/src/view/TodoListView.java
+++ aplikasi-todo-list/src/view/TodoListView.java
@@ -1,8 +1,36 @@
 package view;
 
-public class TodoListView {
-    public void showTodoList(){
+import service.TodoListService;
+import util.InputUtil;
 
+public class TodoListView {
+
+    private TodoListService todoListService;
+
+    public TodoListView(TodoListService todoListService) {
+        this.todoListService = todoListService;
+    }
+
+    public void showTodoList(){
+        while (true) {
+            todoListService.showTodoList();
+
+            System.out.println("MENU : ");
+            System.out.println("1. Tambah");
+            System.out.println("2. Hapus");
+            System.out.println("X. Keluar");
+
+            var input = InputUtil.input("Pilih");
+            if (input.equals("1")) {
+                addTodoList();
+            } else if (input.equals("2")) {
+                removeTodoList();
+            } else if (input.equals("X")) {
+                break;
+            } else {
+                System.out.println("Pilihan tidak di mengerti");
+            }
+        }
     }
     public void addTodoList(){
 
