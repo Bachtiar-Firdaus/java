f3cd9ac988d330586733742a347e1a580c72026d
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-02-24 14:02:28 +0700

    Studi Kasus Java OOP: Aplikasi Todolist 6 # Repository & Service: Menambahkan TodoList

--- aplikasi-todo-list/src/repository/TodoListRepositoryImpl.java
+++ aplikasi-todo-list/src/repository/TodoListRepositoryImpl.java
@@ -11,9 +11,40 @@ public class TodoListRepositoryImpl implements TodoListRepository{
         return data;
     }
 
+    public boolean isFull(){
+        var isFull = true;
+        for (var i = 0; i < data.length; i++){
+            if (data[i] == null){
+                isFull = false;
+                break;
+            }
+        }
+        return isFull;
+    }
+
+    public void resizeIsfull(){
+        // jika penuh, kita lakukan resize ukuran array 2x lipat
+        if(isFull()){
+            var temp = data;
+            data = new TodoList[data.length * 2];
+
+            for (var i = 0; i < temp.length; i++) {
+                data[i] = temp[i];
+            }
+        }
+    }
+
     @Override
     public void add(TodoList todoList) {
+        resizeIsfull();
 
+        // tambahkan ke posisi yang data arraynya null
+        for (var i = 0; i < data.length; i++){
+            if (data[i] == null){
+                data[i] = todoList;
+                break;
+            }
+        }
     }
 
     @Override
