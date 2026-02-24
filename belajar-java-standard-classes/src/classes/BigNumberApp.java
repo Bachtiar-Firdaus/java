390d000c7d91a39b0dd9459f0b739fbb7c4ba062
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-02-24 10:57:22 +0700

    Java Standard Classes 6 # BigNumber Class

---
+++ belajar-java-standard-classes/src/classes/BigNumberApp.java
@@ -0,0 +1,13 @@
+package classes;
+
+import java.math.BigInteger;
+
+public class BigNumberApp {
+    static void main(String[] args) {
+        BigInteger a = new BigInteger("100000000000000000000");
+        BigInteger b = new BigInteger("100000000000000000000");
+
+        BigInteger c  = a.add(b);
+        System.out.println(c);
+    }
+}
