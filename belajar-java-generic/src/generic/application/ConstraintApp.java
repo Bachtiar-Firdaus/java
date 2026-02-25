b2a4d697d2e2314c48a0286c16a5c03930ef5393
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-02-25 11:05:43 +0700

    Java Generic 5 # Bounded Type Parameter

---
+++ belajar-java-generic/src/generic/application/ConstraintApp.java
@@ -0,0 +1,31 @@
+package generic.application;
+
+public class ConstraintApp {
+    static void main(String[] args) {
+
+        NumberData<Integer> integerNumberData = new NumberData<>(1);
+        NumberData<Long> longNumberData = new NumberData<>(1L);
+
+//        NumberData<String> stringNumberData = new NumberData<String>("Daus"); // Error
+
+
+    }
+
+    // Object untuk keseluruhan
+    // Bisa di perkecil dengan spesifict contoh number / string
+    public static class NumberData<T extends Number> {
+        private T data;
+
+        public NumberData(T data) {
+            this.data = data;
+        }
+
+        public T getData() {
+            return data;
+        }
+
+        public void setData(T data) {
+            this.data = data;
+        }
+    }
+}
