3ef9793f4606c8b1bc32717f6a1c8c82f41a34a7
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-02-26 19:28:39 +0700

    Java Lambda 2 # Membuat Lambda

--- belajar-java-lambda/src/lambda/app/SimpleActionApp.java
+++ belajar-java-lambda/src/lambda/app/SimpleActionApp.java
@@ -5,22 +5,35 @@ import lambda.SimpleAction;
 public class SimpleActionApp {
     static void main(String[] args) {
 
-        // versi normal
-        SimpleAction simpleAction = new SimpleAction() {
-            @Override
-            public String action() {
-                return "Daus";
-            }
+//        // versi normal
+//        SimpleAction simpleAction = new SimpleAction() {
+//            @Override
+//            public String action(String name) {
+//                return "Daus";
+//            }
+//        };
+//
+//        System.out.println(simpleAction.action("dika"));
+//
+//        // versi lambda
+//        SimpleAction simpleAction1 = (String name) -> {
+//            return "bachtiar";
+//        };
+//
+//        System.out.println(simpleAction1.action("dani"));
+
+        // ini lambda dengan block
+        SimpleAction simpleAction1 = (String value) -> {
+            return "Hello "+ value;
+        };
+        SimpleAction simpleAction2 = (name) -> {
+            return "Hello "+ name;
         };
 
-        System.out.println(simpleAction.action());
-
-        // versi lambda
-        SimpleAction simpleAction1 = () -> {
-            return "bachtiar";
-        };
-
-        System.out.println(simpleAction1.action());
+        // ini lambda tanpa block
+        SimpleAction simpleAction11 = (String value) -> "Hello "+ value;
+        SimpleAction simpleAction22 = (name) -> "Hello "+ name;
+        SimpleAction simpleAction33 = name -> "Hello "+ name;
 
     }
 }
