1a8827b837481d4168ad25a053c9b93f8c67fdc7
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-02-28 12:02:36 +0700

    Java Unit Test 3 # Menggunakan Assertions

--- belajar-java-unit-test/src/main/java/belajar/java/test/Calculator.java
+++ belajar-java-unit-test/src/main/java/belajar/java/test/Calculator.java
@@ -2,6 +2,14 @@ package belajar.java.test;
 
 public class Calculator {
 
+    public Integer divide(Integer first, Integer second){
+        if(second == 0){
+            throw new IllegalArgumentException("Can't divide by zero");
+        } else {
+            return first / second;
+        }
+    }
+
     public Integer add(Integer first, Integer second){
         return first + second;
     }
--- belajar-java-unit-test/src/test/java/belajar/java/test/CalculatorTest.java
+++ belajar-java-unit-test/src/test/java/belajar/java/test/CalculatorTest.java
@@ -1,6 +1,7 @@
 package belajar.java.test;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
     private Calculator calculator = new Calculator();
@@ -8,5 +9,23 @@ public class CalculatorTest {
     @Test
     public void testAddSuccess(){
         var result  = calculator.add(10,10);
+        assertEquals(20,result);
+        // manual tanpa asertion
+//        if(result != 20){
+//            throw new RuntimeException("Test gagal");
+//        }
+    }
+
+    @Test
+    public void testDivideSuccess(){
+        var result  = calculator.divide(10,10);
+        assertEquals(1,result);
+    }
+
+    @Test
+    public void testDivideFailed(){
+        assertThrows(IllegalArgumentException.class, () -> {
+            calculator.divide(10,0);
+        });
     }
 }
