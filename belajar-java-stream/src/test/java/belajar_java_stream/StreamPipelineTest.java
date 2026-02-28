a804aa5382b3797d10c3292a93009a5ae319a610
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-02-28 20:35:30 +0700

    Java Stream 4 # Stream Pipeline

---
+++ belajar-java-stream/src/test/java/belajar_java_stream/StreamPipelineTest.java
@@ -0,0 +1,35 @@
+package belajar_java_stream;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamPipelineTest {
+
+    @Test
+    void testCreateStreamPipeline (){
+        // ini jarang di gunakan
+        List<String> list = List.of("bach","tiar","fir","daus");
+
+        Stream<String> stream = list.stream();
+
+        Stream<String> streamUpper = stream.map(name -> name.toUpperCase());
+
+        Stream<String> streamMr = streamUpper.map(upper -> "MR. " + upper);
+
+        streamMr.forEach(System.out::println);
+    }
+
+    @Test
+    void testCreateStreamPipelineOk(){
+        List<String> list = List.of("bach","tiar","fir","daus");
+        
+        list.stream()
+                .map(name -> name.toUpperCase())
+                .map(upper -> "MR. " + upper)
+                .forEach(System.out::println);
+
+
+    }
+}
