0f2434ed62a9d4e04aa0da5036625a4113d679b5
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-02-28 15:44:40 +0700

    Java Unit Test 17 # Test Berulang

--- belajar-java-unit-test/src/test/java/belajar/java/test/RandomCalculatorTest.java
+++ belajar-java-unit-test/src/test/java/belajar/java/test/RandomCalculatorTest.java
@@ -1,8 +1,7 @@
 package belajar.java.test;
 
 import belajar.java.test.resolver.RandomParameterResolver;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.extension.Extensions;
 
@@ -13,6 +12,20 @@ public class RandomCalculatorTest extends AbstractCalculatorTest{
 
     @Test
     void testRandom(Random random){
+        var a = random.nextInt();
+        var b = random.nextInt();
+
+        var rest = calculator.add(a,b);
+        var expected = a + b;
+
+        Assertions.assertEquals(expected, rest);
+    }
+
+    @DisplayName("Test Random Calculator")
+    @RepeatedTest(value = 10, name = "{displayName} ke {currentRepetition} dari {totalRepetitions}"
+    )
+    void testRandomRepeat(TestInfo info, Random random, RepetitionInfo repetitionInfo){
+        System.out.println(info.getDisplayName() + " ke " + repetitionInfo.getCurrentRepetition() + " dari " + repetitionInfo.getTotalRepetitions());
         var a = random.nextInt();
         var b = random.nextInt();
 
