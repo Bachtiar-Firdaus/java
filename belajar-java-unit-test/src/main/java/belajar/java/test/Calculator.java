23a0278a301c2549349e4ce1b9491afef674d610
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-02-28 11:36:21 +0700

    Java Unit Test 2 # Membuat Test

---
+++ belajar-java-unit-test/src/main/java/belajar/java/test/Calculator.java
@@ -0,0 +1,9 @@
+package belajar.java.test;
+
+public class Calculator {
+
+    public Integer add(Integer first, Integer second){
+        return first + second;
+    }
+
+}
---
+++ belajar-java-unit-test/src/test/java/belajar/java/test/CalculatorTest.java
@@ -0,0 +1,12 @@
+package belajar.java.test;
+
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+    private Calculator calculator = new Calculator();
+
+    @Test
+    public void testAddSuccess(){
+        var result  = calculator.add(10,10);
+    }
+}
