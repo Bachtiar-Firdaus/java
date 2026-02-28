4835f69388ab0fd6425c2567669ff3f8b9ec4e78
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-02-28 12:24:26 +0700

    Java Unit Test 6 # Sebelum dan Setelah Test

--- belajar-java-unit-test/src/test/java/belajar/java/test/CalculatorTest.java
+++ belajar-java-unit-test/src/test/java/belajar/java/test/CalculatorTest.java
@@ -1,16 +1,39 @@
 package belajar.java.test;
 
 import belajar.java.test.generator.SimpleDisplayNameGenerator;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.DisplayNameGeneration;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayNameGeneration(SimpleDisplayNameGenerator.class)
 //@DisplayName("Test Untuk Calculator Class")
 public class CalculatorTest {
     private Calculator calculator = new Calculator();
+
+    @BeforeAll
+    public static void beforeAll(){
+        System.out.println("Before All");
+    }
+
+    @AfterAll
+    public static void afterAll(){
+        System.out.println("After All");
+    }
+
+    @BeforeEach
+    public void setUp(){
+        System.out.println("Before Each");
+    }
+
+    @AfterEach
+    public void tearDown(){
+        System.out.println("After Each");
+    }
+
+
+
+
+
 
     @Test
 //    @DisplayName("Test Skenario Sukses untuk method add(integer, integer)")
