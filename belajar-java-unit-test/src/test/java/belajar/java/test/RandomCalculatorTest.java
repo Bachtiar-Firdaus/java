b19f3d881b669a5138ee07341a0290df4c447527
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-02-28 15:24:59 +0700

    Java Unit Test 15 # Dependency Injection di Test

---
+++ belajar-java-unit-test/src/test/java/belajar/java/test/RandomCalculatorTest.java
@@ -0,0 +1,28 @@
+package belajar.java.test;
+
+import belajar.java.test.resolver.RandomParameterResolver;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.Extensions;
+
+import java.util.Random;
+
+@Extensions({
+        @ExtendWith(RandomParameterResolver.class)
+})
+public class RandomCalculatorTest {
+
+    private Calculator calculator = new Calculator();
+
+    @Test
+    void testRandom(Random random){
+        var a = random.nextInt();
+        var b = random.nextInt();
+
+        var rest = calculator.add(a,b);
+        var expected = a + b;
+
+        Assertions.assertEquals(expected, rest);
+    }
+}
