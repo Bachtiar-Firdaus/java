3183d094f19302492ebe6cff4cd2316ee75d997b
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-03-02 11:27:30 +0700

    Studi Kasus Java Database : Aplikasi Todolist 5 # Test Repository : Menambah Todolist

--- studi-kasus-java-database-aplikasi-todolist/src/main/java/test/view/TodoListViewTest.java
+++ studi-kasus-java-database-aplikasi-todolist/src/main/java/test/view/TodoListViewTest.java
@@ -14,42 +14,42 @@ public class TodoListViewTest {
     }
 
     public static void testShowTodoList(){
-        TodoListRepository todoListRepository = new TodoListRepositoryImpl();
-        TodoListService todoListService = new TodoListServiceImpl(todoListRepository);
-        TodoListView todoListView = new TodoListView(todoListService);
-
-        todoListService.addTodoList("Belajar Java Dasar");
-        todoListService.addTodoList("Belajar Java OOP");
-        todoListService.addTodoList("Belajar Java SpringBoot");
-
-        todoListView.showTodoList();
+//        TodoListRepository todoListRepository = new TodoListRepositoryImpl();
+//        TodoListService todoListService = new TodoListServiceImpl(todoListRepository);
+//        TodoListView todoListView = new TodoListView(todoListService);
+//
+//        todoListService.addTodoList("Belajar Java Dasar");
+//        todoListService.addTodoList("Belajar Java OOP");
+//        todoListService.addTodoList("Belajar Java SpringBoot");
+//
+//        todoListView.showTodoList();
     }
 
 
     public static void testAddTodoList(){
-        TodoListRepository todoListRepository = new TodoListRepositoryImpl();
-        TodoListService todoListService = new TodoListServiceImpl(todoListRepository);
-        TodoListView todoListView = new TodoListView(todoListService);
-
-        todoListView.addTodoList();
-        todoListService.showTodoList();
-        todoListView.addTodoList();
+//        TodoListRepository todoListRepository = new TodoListRepositoryImpl();
+//        TodoListService todoListService = new TodoListServiceImpl(todoListRepository);
+//        TodoListView todoListView = new TodoListView(todoListService);
+//
+//        todoListView.addTodoList();
+//        todoListService.showTodoList();
+//        todoListView.addTodoList();
     }
 
 
 
     public static void testRemoveTodoList(){
-        TodoListRepository todoListRepository = new TodoListRepositoryImpl();
-        TodoListService todoListService = new TodoListServiceImpl(todoListRepository);
-        TodoListView todoListView = new TodoListView(todoListService);
-
-        todoListService.addTodoList("Belajar Java Dasar");
-        todoListService.addTodoList("Belajar Java OOP");
-        todoListService.addTodoList("Belajar Java SpringBoot");
-
-        todoListService.showTodoList();
-        todoListView.removeTodoList();
-        todoListService.showTodoList();
+//        TodoListRepository todoListRepository = new TodoListRepositoryImpl();
+//        TodoListService todoListService = new TodoListServiceImpl(todoListRepository);
+//        TodoListView todoListView = new TodoListView(todoListService);
+//
+//        todoListService.addTodoList("Belajar Java Dasar");
+//        todoListService.addTodoList("Belajar Java OOP");
+//        todoListService.addTodoList("Belajar Java SpringBoot");
+//
+//        todoListService.showTodoList();
+//        todoListView.removeTodoList();
+//        todoListService.showTodoList();
 
     }
 }
