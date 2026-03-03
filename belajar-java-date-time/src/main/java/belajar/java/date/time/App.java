b2f286ed5137fc21b70516883a6978dcb351ab4b
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-03-03 19:58:48 +0700

    Java Date & Time 1 # Date

---
+++ belajar-java-date-time/src/main/java/belajar/java/date/time/App.java
@@ -0,0 +1,13 @@
+package belajar.java.date.time;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ belajar-java-date-time/src/test/java/belajar/java/date/time/DateTest.java
@@ -0,0 +1,23 @@
+package belajar.java.date.time;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
+public class DateTest {
+
+    @Test
+    void create(){
+
+        var date1 = new Date();
+        var date2 = new Date(System.currentTimeMillis());
+        var date3 = new Date(879872400000L);
+
+
+        System.out.println(date1);
+        System.out.println(date2);
+        System.out.println(date3);
+
+    }
+
+}
