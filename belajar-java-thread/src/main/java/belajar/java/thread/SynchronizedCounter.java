119beb878867bd8e590da1e8f78d1a98b43c0b28
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-03-04 12:32:21 +0700

    Java Thread 4 # Synchronization

---
+++ belajar-java-thread/src/main/java/belajar/java/thread/SynchronizedCounter.java
@@ -0,0 +1,14 @@
+package belajar.java.thread;
+
+public class SynchronizedCounter {
+    private Long value = 0L;
+    public  void increment(){
+        synchronized (this) {
+            value++;
+        }
+    }
+
+    public Long getValue(){
+        return value;
+    }
+}
