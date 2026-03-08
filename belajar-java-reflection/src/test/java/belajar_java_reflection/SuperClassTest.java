75dd1b92ec4cce79014e73f25ff121c735460993
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-03-08 17:48:09 +0700

    Java Reflection 7 # Super Class

---
+++ belajar-java-reflection/src/test/java/belajar_java_reflection/SuperClassTest.java
@@ -0,0 +1,21 @@
+package belajar_java_reflection;
+
+import belajar_java_reflection.data.Person;
+import org.junit.jupiter.api.Test;
+
+public class SuperClassTest {
+
+    @Test
+    void testGetSuperClass() {
+
+        Class<Person> personClass = Person.class;
+        System.out.println(personClass);
+
+        Class<? super Person> objectClass = personClass.getSuperclass();
+        System.out.println(objectClass);
+
+        Class<? super Person> nullClass = objectClass.getSuperclass();
+        System.out.println(nullClass);
+
+    }
+}
