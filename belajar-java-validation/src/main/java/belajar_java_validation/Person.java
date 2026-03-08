885e163bbf0e5453de61d3498fbfe7a44105784b
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-03-08 22:12:04 +0700

    Java Validation 3 # Constraint

---
+++ belajar-java-validation/src/main/java/belajar_java_validation/Person.java
@@ -0,0 +1,49 @@
+package belajar_java_validation;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class Person {
+
+    @Size(max = 20, message = "first name length max must 20 characters")
+    @NotBlank(message = "first name can't blank")
+    private String firstName;
+
+    @Size(max = 20, message = "last name length max must 20 characters")
+    @NotBlank(message = "last name can't blank")
+    private String lastName;
+
+
+    
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Person() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
