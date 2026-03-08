9c2e212ce172202c518143aae1b334efff5cfa80
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-03-08 22:20:33 +0700

    Java Validation 4 # Constraint Validator

--- belajar-java-validation/src/main/java/belajar_java_validation/Person.java
+++ belajar-java-validation/src/main/java/belajar_java_validation/Person.java
@@ -14,7 +14,7 @@ public class Person {
     private String lastName;
 
 
-    
+
     public Person(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
