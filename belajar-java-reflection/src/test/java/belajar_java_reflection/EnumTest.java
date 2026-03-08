1eb9de41bd9dc939b0245114d750e559c3ff6687
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-03-08 19:33:28 +0700

    Java Reflection 12 # Enum

---
+++ belajar-java-reflection/src/test/java/belajar_java_reflection/EnumTest.java
@@ -0,0 +1,25 @@
+package belajar_java_reflection;
+
+
+import belajar_java_reflection.data.Gender;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+public class EnumTest {
+
+    @Test
+    void testEnum() {
+
+        Class<Gender> genderClass = Gender.class;
+
+        System.out.println(genderClass.getName());
+        System.out.println(genderClass.getSuperclass());
+        System.out.println(Arrays.toString(genderClass.getEnumConstants()));
+        System.out.println(genderClass.isEnum());
+        System.out.println(Arrays.toString(genderClass.getDeclaredFields()));
+        System.out.println(Arrays.toString(genderClass.getDeclaredConstructors()));
+        System.out.println(Arrays.toString(genderClass.getDeclaredMethods()));
+
+    }
+}
