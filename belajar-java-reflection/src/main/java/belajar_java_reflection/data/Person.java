9dc76e9b58e8f425290cd06f264963294c5d2e7b
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-03-08 18:01:24 +0700

    Java Reflection 8 # Interface

--- belajar-java-reflection/src/main/java/belajar_java_reflection/data/Person.java
+++ belajar-java-reflection/src/main/java/belajar_java_reflection/data/Person.java
@@ -1,6 +1,8 @@
 package belajar_java_reflection.data;
 
-public class Person {
+import java.io.Serializable;
+
+public class Person implements Nameable, Serializable {
     private  String firstName;
     private String lastName;
 
