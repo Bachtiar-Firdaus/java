5d2cace73d9da9c2eec00572d0d0a898b1c73bc7
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-03-10 15:45:36 +0700

    Java Lombok 14 # Synchronized

---
+++ belajar-java-lombok/src/main/java/bachtiar_firdaus/belajar_java_lombok/Counter.java
@@ -0,0 +1,21 @@
+package bachtiar_firdaus.belajar_java_lombok;
+
+import lombok.Synchronized;
+
+public class Counter {
+
+    private final Object counterLock = new Object();
+
+    private Long counter = 0L;
+
+    @Synchronized(value = "counterLock")
+    public void increment() {
+        counter = counter + 1;
+    }
+
+    @Synchronized(value = "counterLock")
+    public Long getCounter() {
+        return counter;
+    }
+
+}
