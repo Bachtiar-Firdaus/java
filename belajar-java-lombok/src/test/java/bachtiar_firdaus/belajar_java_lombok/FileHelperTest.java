c4ddef7f2331907b1a173a73300ed56318573595
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-03-10 15:34:23 +0700

    Java Lombok 11 # CleanUp

---
+++ belajar-java-lombok/src/test/java/bachtiar_firdaus/belajar_java_lombok/FileHelperTest.java
@@ -0,0 +1,22 @@
+package bachtiar_firdaus.belajar_java_lombok;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+
+public class FileHelperTest {
+
+    @Test
+    void testPom() throws Exception {
+        String text = FileHelper.loadFile("pom.xml");
+        System.out.println(text);
+    }
+
+//    @Test
+//    void testFileNotFound() {
+//        Assertions.assertThrows(FileNotFoundException.class, () -> {
+//            FileHelper.loadFile("tidakada.txt");
+//        });
+//    }
+}
