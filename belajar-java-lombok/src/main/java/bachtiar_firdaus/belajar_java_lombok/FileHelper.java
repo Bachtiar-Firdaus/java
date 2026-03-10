cabfdea3b0507d037f12ab16ba70b82c166ac5e7
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-03-10 15:40:59 +0700

    Java Lombok 13 # Log

--- belajar-java-lombok/src/main/java/bachtiar_firdaus/belajar_java_lombok/FileHelper.java
+++ belajar-java-lombok/src/main/java/bachtiar_firdaus/belajar_java_lombok/FileHelper.java
@@ -2,18 +2,18 @@ package bachtiar_firdaus.belajar_java_lombok;
 
 import lombok.Cleanup;
 import lombok.SneakyThrows;
-//import lombok.extern.slf4j.Slf4j;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.FileReader;
 import java.util.Scanner;
 
-//@Slf4j
+@Slf4j
 public class FileHelper {
 
     // ini unuk mengakali try catch SneakyThrows
     @SneakyThrows
     public static String loadFile(String file) {
-//        log.info("Load file {}", file);
+        log.info("Load file {}", file);
         @Cleanup FileReader fileReader = new FileReader(file);
         @Cleanup Scanner scanner = new Scanner(fileReader);
 
