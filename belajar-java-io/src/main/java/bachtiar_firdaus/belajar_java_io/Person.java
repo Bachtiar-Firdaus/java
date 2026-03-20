1235d4642c02128ae9421fbd5e2d57effa6c5ebf
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-03-20 13:07:34 +0700

    Java IO 14 # Object Stream

---
+++ belajar-java-io/src/main/java/bachtiar_firdaus/belajar_java_io/Person.java
@@ -0,0 +1,29 @@
+package bachtiar_firdaus.belajar_java_io;
+
+
+import java.io.Serializable;
+
+public class Person implements Serializable {
+
+    public static final long serialVersionUID = 1L;
+
+    private String id;
+
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ belajar-java-io/src/test/java/bachtiar_firdaus/belajar_java_io/ObjectStreamTest.java
@@ -0,0 +1,47 @@
+package bachtiar_firdaus.belajar_java_io;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class ObjectStreamTest {
+
+    @Test
+    void savePerson() {
+        Person person = new Person();
+        person.setId("1");
+        person.setName("Daus");
+
+        Path path = Path.of("daus.person");
+
+        try(OutputStream stream = Files.newOutputStream(path);
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(stream)){
+
+            objectOutputStream.writeObject(person);
+            objectOutputStream.flush();
+
+        }catch (IOException exception){
+            Assertions.fail(exception);
+        }
+    }
+
+    @Test
+    void getPerson() {
+        Path path = Path.of("daus.person");
+
+        try(InputStream stream = Files.newInputStream(path);
+            ObjectInputStream objectInputStream = new ObjectInputStream(stream)){
+
+            Person person = (Person) objectInputStream.readObject();
+
+            Assertions.assertEquals("1", person.getId());
+            Assertions.assertEquals("Daus", person.getName());
+
+        }catch (IOException | ClassNotFoundException exception){
+            Assertions.fail(exception);
+        }
+    }
+}
