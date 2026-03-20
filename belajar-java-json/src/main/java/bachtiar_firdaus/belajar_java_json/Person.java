a90599b76ac6bd8ca7d6717d8b6efb8b7c67e53d
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-03-20 15:42:11 +0700

    Java JSON 12 # Jackson Annotation

--- belajar-java-json/src/main/java/bachtiar_firdaus/belajar_java_json/Person.java
+++ belajar-java-json/src/main/java/bachtiar_firdaus/belajar_java_json/Person.java
@@ -1,6 +1,10 @@
 package bachtiar_firdaus.belajar_java_json;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.Date;
 import java.util.List;
 
@@ -10,13 +14,36 @@ public class Person {
 
     private String name;
 
+    @JsonProperty("full_name")
+    private String fullName;
+
+    @JsonIgnore
+    private String password;
+
     private List<String> hobbies;
 
     private Address address;
 
     private Date createdAt;
 
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private Date updatedAt;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     public Date getCreatedAt() {
         return createdAt;
---
+++ belajar-java-json/src/test/java/bachtiar_firdaus/belajar_java_json/AnnotationTest.java
@@ -0,0 +1,33 @@
+package bachtiar_firdaus.belajar_java_json;
+
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.junit.jupiter.api.Test;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class AnnotationTest {
+
+    @Test
+    void annotation() throws JsonProcessingException {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        ObjectMapper objectMapper = new ObjectMapper()
+                .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+                .setDateFormat(dateFormat);
+
+        Person person = new Person();
+        person.setId("1");
+        person.setName("Daus");
+        person.setFullName("Bachtiar Firdaus");
+        person.setPassword("rahasia");
+        person.setCreatedAt(new Date());
+        person.setUpdatedAt(new Date());
+
+        String json = objectMapper.writeValueAsString(person);
+        System.out.println(json);
+    }
+}
