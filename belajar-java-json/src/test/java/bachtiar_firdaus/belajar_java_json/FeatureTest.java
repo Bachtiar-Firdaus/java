d303b12dee1cc8e2491b0e52e94034bd7e4dfd7f
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-03-20 15:19:25 +0700

    Java JSON 7 # Mapper Feature

---
+++ belajar-java-json/src/test/java/bachtiar_firdaus/belajar_java_json/FeatureTest.java
@@ -0,0 +1,31 @@
+package bachtiar_firdaus.belajar_java_json;
+
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class FeatureTest {
+
+    @Test
+    void mapperFeature() throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper()
+                .configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true);
+
+        String json = """
+                {"ID" : "1", "Name": "Eko"}
+                """;
+
+        Person person = objectMapper.readValue(json, Person.class);
+        Assertions.assertEquals("1", person.getId());
+        Assertions.assertEquals("Eko", person.getName());
+    }
+
+}
