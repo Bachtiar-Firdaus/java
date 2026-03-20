c0688f4ce413af66071774ea60311dd9842548db
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-03-20 13:18:45 +0700

    Java IO 17 # Scanner IMPL APP

---
+++ belajar-java-io/src/main/java/bachtiar_firdaus/belajar_java_io/InputApp.java
@@ -0,0 +1,17 @@
+package bachtiar_firdaus.belajar_java_io;
+
+
+import java.util.Scanner;
+
+public class InputApp {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan nama: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Halo " + name);
+
+    }
+}
