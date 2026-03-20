fba9c35b0a0a67362cc4115de48bc79ce095e929
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-03-20 13:35:41 +0700

    Java JSON 2 # Object Mapper

---
+++ belajar-java-json/src/test/java/bachtiar_firdaus/belajar_java_json/ObjectMapperTest.java
@@ -0,0 +1,15 @@
+package bachtiar_firdaus.belajar_java_json;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ObjectMapperTest {
+
+    @Test
+    void createObjectMapper(){
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        Assertions.assertNotNull(objectMapper);
+    }
+}
