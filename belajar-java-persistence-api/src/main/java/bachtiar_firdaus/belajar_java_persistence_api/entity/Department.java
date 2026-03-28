51d1befe699bec180b44a6bd6795b442791b9dbe
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-03-28 16:45:05 +0700

    Java Presistence Api 15 # Embedded Id

---
+++ belajar-java-persistence-api/src/main/java/bachtiar_firdaus/belajar_java_persistence_api/entity/Department.java
@@ -0,0 +1,31 @@
+package bachtiar_firdaus.belajar_java_persistence_api.entity;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "departments")
+public class Department {
+
+    @EmbeddedId
+    private DepartmentId id;
+
+    private String name;
+
+    public DepartmentId getId() {
+        return id;
+    }
+
+    public void setId(DepartmentId id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- belajar-java-persistence-api/src/test/java/bachtiar_firdaus/belajar_java_persistence_api/EmbeddedTest.java
+++ belajar-java-persistence-api/src/test/java/bachtiar_firdaus/belajar_java_persistence_api/EmbeddedTest.java
@@ -1,6 +1,8 @@
 package bachtiar_firdaus.belajar_java_persistence_api;
 
 
+import bachtiar_firdaus.belajar_java_persistence_api.entity.Department;
+import bachtiar_firdaus.belajar_java_persistence_api.entity.DepartmentId;
 import bachtiar_firdaus.belajar_java_persistence_api.entity.Member;
 import bachtiar_firdaus.belajar_java_persistence_api.entity.Name;
 import bachtiar_firdaus.belajar_java_persistence_api.util.JpaUtil;
@@ -35,5 +37,42 @@ public class EmbeddedTest {
         entityTransaction.commit();
         entityManager.close();
     }
+    @Test
+    void embeddedId() {
+        EntityManagerFactory entityManagerFactory = JpaUtil.getEntityManagerFactory();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
 
+        DepartmentId id = new DepartmentId();
+        id.setCompanyId("pzn");
+        id.setDepartmentId("tech");
+
+        Department department = new Department();
+        department.setId(id);
+        department.setName("Teknologi");
+
+        entityManager.persist(department);
+
+        entityTransaction.commit();
+        entityManager.close();
+    }
+
+    @Test
+    void embeddedIdFind() {
+        EntityManagerFactory entityManagerFactory = JpaUtil.getEntityManagerFactory();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+
+        DepartmentId id = new DepartmentId();
+        id.setCompanyId("pzn");
+        id.setDepartmentId("tech");
+
+        Department department = entityManager.find(Department.class, id);
+        Assertions.assertEquals("Teknologi", department.getName());
+
+        entityTransaction.commit();
+        entityManager.close();
+    }
 }
