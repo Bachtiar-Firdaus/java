d16fa80b807c84e7b7c11efa29f7982a5ea09938
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-03-28 12:22:37 +0700

    Java Web Servlet 14 # Upload File

---
+++ belajar-java-web-servlet/src/main/java/bachtiar_firdaus/belajar_java_web_servlet/FormUploadServlet.java
@@ -0,0 +1,56 @@
+package bachtiar_firdaus.belajar_java_web_servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Part;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.UUID;
+
+@WebServlet(urlPatterns = "/form-upload")
+@MultipartConfig
+public class FormUploadServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // Baca file HTML dari resources dengan getResourceAsStream
+        try (var inputStream = FormUploadServlet.class.getResourceAsStream("/html/form-upload.html")) {
+            if (inputStream == null) {
+                resp.getWriter().println("File form-upload.html tidak ditemukan!");
+                return;
+            }
+            String html = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+            resp.getWriter().println(html);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        Part profile = req.getPart("profile");
+
+        Path uploadLocation = Path.of("upload/" + UUID.randomUUID().toString() + profile.getSubmittedFileName());
+        Files.copy(profile.getInputStream(), uploadLocation);
+
+        String html = """
+                <html>
+                <body>
+                Name : $name
+                <br>
+                Profile : <img width="400px" height="400px" src="/download?file=$profile" />
+                </body>
+                <html>
+                """
+                .replace("$name", name)
+                .replace("$profile", uploadLocation.getFileName().toString());
+
+        resp.getWriter().println(html);
+    }
+}
