bf884042c7201008bb3ee32378a38293c0cbf8d0
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-03-28 15:46:50 +0700

    Java Presistence Api 8 # Generated Value

---
+++ belajar-java-persistence-api/src/test/java/bachtiar_firdaus/belajar_java_persistence_api/GeneratedValueTest.java
@@ -0,0 +1,31 @@
+package bachtiar_firdaus.belajar_java_persistence_api;
+
+import bachtiar_firdaus.belajar_java_persistence_api.entity.Category;
+import bachtiar_firdaus.belajar_java_persistence_api.util.JpaUtil;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class GeneratedValueTest {
+
+    @Test
+    void generatedValue() {
+        EntityManagerFactory entityManagerFactory = JpaUtil.getEntityManagerFactory();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+
+        Category category = new Category();
+        category.setName("Gadget");
+        category.setDescription("Gadget Termurah");
+        entityManager.persist(category);
+
+        Assertions.assertNotNull(category.getId());
+
+        entityTransaction.commit();
+        entityManager.close();
+    }
+}
