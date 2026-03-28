781da7ad44288049a114f35cc4ecff50fc06e4ab
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-03-28 15:39:36 +0700

    Java Presistence Api 7 # Column

---
+++ belajar-java-persistence-api/src/test/java/bachtiar_firdaus/belajar_java_persistence_api/ColumnTest.java
@@ -0,0 +1,30 @@
+package bachtiar_firdaus.belajar_java_persistence_api;
+
+
+import bachtiar_firdaus.belajar_java_persistence_api.entity.Customer;
+import bachtiar_firdaus.belajar_java_persistence_api.util.JpaUtil;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import org.junit.jupiter.api.Test;
+
+public class ColumnTest {
+
+    @Test
+    void column() {
+        EntityManagerFactory entityManagerFactory = JpaUtil.getEntityManagerFactory();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+
+        Customer customer = new Customer();
+        customer.setId("1");
+        customer.setName("daus");
+        customer.setPrimaryEmail("contoh@example.com");
+
+        entityManager.persist(customer);
+
+        entityTransaction.commit();
+        entityManager.close();
+    }
+}
