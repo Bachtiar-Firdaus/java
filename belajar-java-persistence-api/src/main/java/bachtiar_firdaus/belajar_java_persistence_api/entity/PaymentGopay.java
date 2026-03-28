612b03eee710bb1fd45b40c7f2b248ff1e72d4ae
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-03-28 19:24:33 +0700

    Java Presistence Api 25 # Joined Table Inheritance

---
+++ belajar-java-persistence-api/src/main/java/bachtiar_firdaus/belajar_java_persistence_api/entity/PaymentGopay.java
@@ -0,0 +1,21 @@
+package bachtiar_firdaus.belajar_java_persistence_api.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "payments_gopay")
+public class PaymentGopay extends Payment{
+
+    @Column(name = "gopay_id")
+    private String gopayId;
+
+    public String getGopayId() {
+        return gopayId;
+    }
+
+    public void setGopayId(String gopayId) {
+        this.gopayId = gopayId;
+    }
+}
--- belajar-java-persistence-api/src/test/java/bachtiar_firdaus/belajar_java_persistence_api/InheritanceTest.java
+++ belajar-java-persistence-api/src/test/java/bachtiar_firdaus/belajar_java_persistence_api/InheritanceTest.java
@@ -1,8 +1,6 @@
 package bachtiar_firdaus.belajar_java_persistence_api;
 
-import bachtiar_firdaus.belajar_java_persistence_api.entity.Employee;
-import bachtiar_firdaus.belajar_java_persistence_api.entity.Manager;
-import bachtiar_firdaus.belajar_java_persistence_api.entity.VicePresident;
+import bachtiar_firdaus.belajar_java_persistence_api.entity.*;
 import bachtiar_firdaus.belajar_java_persistence_api.util.JpaUtil;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
@@ -56,6 +54,57 @@ public class InheritanceTest {
         Employee employee = entityManager.find(Employee.class, "budi");
         VicePresident vicePresident = (VicePresident) employee;
         Assertions.assertEquals("Budi Nugraha", vicePresident.getName());
+
+        entityTransaction.commit();
+        entityManager.close();
+    }
+
+    @Test
+    void joinedTableInsert() {
+        EntityManagerFactory entityManagerFactory = JpaUtil.getEntityManagerFactory();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+
+        PaymentGopay gopay = new PaymentGopay();
+        gopay.setId("gopay1");
+        gopay.setAmount(100_000L);
+        gopay.setGopayId("089999999999");
+        entityManager.persist(gopay);
+
+        PaymentCreditCard creditCard = new PaymentCreditCard();
+        creditCard.setId("cc1");
+        creditCard.setAmount(500_000L);
+        creditCard.setMaskedCard("4555-5555");
+        creditCard.setBank("BCA");
+        entityManager.persist(creditCard);
+
+        entityTransaction.commit();
+        entityManager.close();
+    }
+
+    @Test
+    void joinedTableFind() {
+        EntityManagerFactory entityManagerFactory = JpaUtil.getEntityManagerFactory();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+
+        PaymentGopay gopay = entityManager.find(PaymentGopay.class, "gopay1");
+        PaymentCreditCard creditCard = entityManager.find(PaymentCreditCard.class, "cc1");
+
+        entityTransaction.commit();
+        entityManager.close();
+    }
+
+    @Test
+    void joinedTableFindParent() {
+        EntityManagerFactory entityManagerFactory = JpaUtil.getEntityManagerFactory();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+
+        Payment gopay = entityManager.find(Payment.class, "gopay1");
 
         entityTransaction.commit();
         entityManager.close();
