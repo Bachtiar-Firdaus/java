d3e219b69461fd7908060dee0fc261b98deea448
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-03-29 17:04:31 +0700

    Java Sealed Class 3 # Non Sealed Class

---
+++ belajar-java-sealed-class/src/main/java/bachtiar_firdaus/belajar_java_sealed_class/data/ManagerOther.java
@@ -0,0 +1,4 @@
+package bachtiar_firdaus.belajar_java_sealed_class.data;
+
+public class ManagerOther extends Manager{
+}
---
+++ belajar-java-sealed-class/src/main/java/bachtiar_firdaus/belajar_java_sealed_class/data/ManagerTest.java
@@ -0,0 +1,5 @@
+package bachtiar_firdaus.belajar_java_sealed_class.data;
+
+
+public final class ManagerTest extends Manager{
+}
