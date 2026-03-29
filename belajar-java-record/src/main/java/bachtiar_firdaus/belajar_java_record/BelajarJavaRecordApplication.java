8ad58d94e1fddcb6dcbc866e9b1bafcaafe26acb
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-03-29 15:47:55 +0700

    Java Record 1 # Membuat Project

---
+++ belajar-java-record/src/main/java/bachtiar_firdaus/belajar_java_record/BelajarJavaRecordApplication.java
@@ -0,0 +1,13 @@
+package bachtiar_firdaus.belajar_java_record;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarJavaRecordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarJavaRecordApplication.class, args);
+	}
+
+}
