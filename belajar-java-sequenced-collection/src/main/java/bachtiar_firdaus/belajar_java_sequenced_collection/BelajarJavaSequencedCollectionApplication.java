1eeeead3e135f730130c18240e12fcb61ee387a7
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-03-29 14:16:38 +0700

    Java Sequenced Collection 1 # Membuat Project

---
+++ belajar-java-sequenced-collection/src/main/java/bachtiar_firdaus/belajar_java_sequenced_collection/BelajarJavaSequencedCollectionApplication.java
@@ -0,0 +1,13 @@
+package bachtiar_firdaus.belajar_java_sequenced_collection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarJavaSequencedCollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarJavaSequencedCollectionApplication.class, args);
+	}
+
+}
---
+++ belajar-java-sequenced-collection/src/test/java/bachtiar_firdaus/belajar_java_sequenced_collection/BelajarJavaSequencedCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package bachtiar_firdaus.belajar_java_sequenced_collection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarJavaSequencedCollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
