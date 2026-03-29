6c082ca55ca9534663340c4cae5247a00c7026eb
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-03-29 14:26:02 +0700

    Java Sequenced Collection 3 # Sequenced Set

---
+++ belajar-java-sequenced-collection/src/test/java/bachtiar_firdaus/belajar_java_sequenced_collection/SetTest.java
@@ -0,0 +1,35 @@
+package bachtiar_firdaus.belajar_java_sequenced_collection;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.SequencedSet;
+import java.util.TreeSet;
+
+public class SetTest {
+
+    @Test
+    void set() {
+
+        SequencedSet<String> set = new TreeSet<>();
+        set.add("Eko"); // eko
+        set.add("Budi"); // budi, eko
+        set.add("Joko"); // budi, eko, joko
+
+        Assertions.assertEquals("Budi", set.getFirst());
+        Assertions.assertEquals("Joko", set.getLast());
+
+        SequencedSet<String> reversed = set.reversed();
+
+        Assertions.assertEquals("Budi", reversed.getLast());
+        Assertions.assertEquals("Joko", reversed.getFirst());
+
+        String value = set.removeFirst();
+        Assertions.assertEquals("Budi", value);
+
+        value = set.removeFirst();
+        Assertions.assertEquals("Eko", value);
+
+    }
+}
