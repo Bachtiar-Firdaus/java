c5f883a50304e28c1ec865267eb6929ce14dfde2
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-03-29 16:53:53 +0700

    Java Sealed Class 1 # Membuat Project

---
+++ belajar-java-sealed-class/src/main/java/bachtiar_firdaus/belajar_java_sealed_class/BelajarJavaSealedClassApplication.java
@@ -0,0 +1,13 @@
+package bachtiar_firdaus.belajar_java_sealed_class;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarJavaSealedClassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarJavaSealedClassApplication.class, args);
+	}
+
+}
---
+++ belajar-java-sealed-class/src/test/java/bachtiar_firdaus/belajar_java_sealed_class/BelajarJavaSealedClassApplicationTests.java
@@ -0,0 +1,13 @@
+package bachtiar_firdaus.belajar_java_sealed_class;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarJavaSealedClassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
